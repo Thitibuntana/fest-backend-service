e79ed981ca16762872235ed58987c7ee1abedd92
Thitibuntana T <s6652410022@sau.ac.th>
2025-03-20 16:12:58 +0700

    G

--- serverservice.js
+++ serverservice.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/fest',festRoute);
-
+app.use('/images', express.static('images'));
 
 app.get('/', (req, res) => {
     res.json({ message: "Hello, welcome to MY server!" })
